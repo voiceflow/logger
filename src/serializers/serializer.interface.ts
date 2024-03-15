b4b772e61fa83e034b1e15980a5af921eca7f531
Tyler Stewart <git@tylerstewart.ca>
2024-03-15 15:33:33 -0600

    fix: types

--- src/serializers/serializer.interface.ts
+++ src/serializers/serializer.interface.ts
@@ -1,7 +1,9 @@
 import { SerializedError, SerializedRequest, SerializedResponse } from 'pino';
 
-export interface Serializer {
-  req: (req: SerializedRequest & { raw: SerializedRequest['raw'] & { originalUrl?: string } }) => any;
-  res: (res: SerializedResponse) => any;
-  err: (err: SerializedError) => any;
-}
+// Must be a "type" to conform to Pino's serializer definition
+/* eslint-disable-next-line @typescript-eslint/consistent-type-definitions */
+export type Serializer = {
+  req(req: SerializedRequest & { raw: SerializedRequest['raw'] & { originalUrl?: string } }): any;
+  res(res: SerializedResponse): any;
+  err(err: SerializedError): any;
+};
